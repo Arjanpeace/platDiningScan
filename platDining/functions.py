052eafa4eecdb78c86e56fff3d7e8911d5abd6f3
Arjanpeace <arjanhoogenboom@hotmail.com>
2024-01-17 18:23:59 +0000

    Adding Google Tag

--- platDining/functions.py
+++ platDining/functions.py
@@ -189,6 +189,29 @@ def createMap(merchants):
         clear_text='Remove Filter'
     ).add_to(m)
 
-    m.save("index.html")
+    google_tag_head = """
+        <head> 
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PHW8JV6B');</script>
+        <!-- End Google Tag Manager -->
+        """
 
+    google_tag_body = """
+        <body>
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHW8JV6B"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+        
+        """
+
+    m = m.get_root().render().replace('<head>', google_tag_head).replace('<body>', google_tag_body)
+
+    text_file = open("index.html", "w")
+    text_file.write(m)
+    text_file.close()
 
