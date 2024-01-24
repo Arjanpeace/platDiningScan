ea8d11b359973ba1df21a54bf2b24e4c8c956532
Arjanpeace <33484997+Arjanpeace@users.noreply.github.com>
2024-01-24 14:58:30 +0000

    Update functions.py

--- platDining/functions.py
+++ platDining/functions.py
@@ -218,7 +218,18 @@ def createInitialMap() -> folium.folium.Map:
     plugins.MiniMap(toggle_display=True).add_to(m)
 
     return m
+    
 
+def addCoffeeDonate(m: str) -> str:
+    current_date = strftime("%Y-%m-%d", gmtime())
+    stringToReplace = f'"Latest Update on {current_date} by SuveBoom. Why not buy me a coffee?"'
+    donateLink = 'https://www.buymeacoffee.com/suveboom'
+    htmlString = f'<a href="{donateLink}" target="_blank">coffee</a>'
+    stringToReplaceCleaned = stringToReplace.replace('"',"'")
+    newString = stringToReplaceCleaned.replace('coffee', htmlString)
+    
+    return m.replace(stringToReplace, newString)
+    
 
 def createMap(merchants: dict):
     current_working_directory = os.getcwd()
@@ -267,6 +278,7 @@ def createMap(merchants: dict):
     ).add_to(m)
 
     m = addGoogleTag(m)
+    #m = addCoffeeDonate(m)
     text_file = open(f'{current_working_directory}/index.html', "w")
     text_file.write(m)
     text_file.close()
